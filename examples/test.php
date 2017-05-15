f9ee374cfcd4b8317498d1583f85a67dc4aa6e8c
Keven <keven@adagiolabs.fr>
2017-05-15 15:02:35 +0200

    Initial commit

---
+++ examples/test.php
@@ -0,0 +1,78 @@
+<?php
+
+require_once __DIR__.'/bootstrap.php';
+
+use Doctrine\DBAL\DriverManager;
+use Adagio\DataStore\Adapter\DbalStore;
+use Adagio\Repository\Repository;
+
+$conn = DriverManager::getConnection(['url' => 'pgsql://repo:repo@localhost/repo']);
+$repository = new Repository(new DbalStore($conn));
+
+class Foo
+{
+    private $bar;
+
+    public function __construct($bar)
+    {
+        $this->bar = $bar;
+    }
+
+    public function bar()
+    {
+        return $this->bar;
+    }
+}
+
+class Baz
+{
+    private $id;
+    private $bar;
+
+    public function __construct($id, $bar)
+    {
+        $this->id = $id;
+        $this->bar = $bar;
+    }
+
+    public function id()
+    {
+        return $this->id;
+    }
+
+    public function bar()
+    {
+        return $this->bar;
+    }
+}
+
+class Bar
+{
+    private $id;
+    private $baz;
+
+    public function __construct($id, $baz)
+    {
+        $this->id = $id;
+        $this->baz = $baz;
+    }
+
+    public function id()
+    {
+        return $this->id;
+    }
+
+    public function baz()
+    {
+        return $this->baz;
+    }
+}
+
+var_dump($k1 = $repository->store(new Foo('yo')));
+var_dump($repository->store(new Baz($k2 = 'id_'.uniqid(), 'yo')));
+var_dump($repository->get($k1));
+var_dump($repository->get($k2));
+var_dump($repository->has($k1));
+$repository->remove($k1);
+$repository->remove($k2);
+var_dump($repository->has($k1));
---
+++ src/Exception/NotFound.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Adagio\Repository\Exception;
+
+final class NotFound extends \OutOfBoundsException
+{
+    /**
+     *
+     * @param string $identifier
+     *
+     * @return NotFound
+     */
+    static public function fromIdentifier($identifier)
+    {
+        return new self("No entity found for identifier '$identifier'.");
+    }
+
+    /**
+     *
+     * @param string $name
+     * @param string $comparator
+     * @param mixed $value
+     *
+     * @return NotFound
+     */
+    static public function fromProperty($name, $comparator, $value)
+    {
+        return new self("No entity found where '$value $comparator $name'.");
+    }
+}
